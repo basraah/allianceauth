f1af57da39d60c890de9834468379f7acd758f4b
Basraah <basraaheve@gmail.com>
2017-01-12 14:41:30 +1000

    Added dynamic user model admin actions for syncing service groups

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,9 +1,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 from authentication.models import AuthServicesInfo
 from eveonline.models import EveCharacter
+from alliance_auth.hooks import get_hooks
+from services.hooks import ServicesHook
 
 
 @admin.register(AuthServicesInfo)
@@ -46,3 +51,43 @@ class AuthServicesInfoManager(admin.ModelAdmin):
         'main_char_id',
     ]
     list_display = ('user', 'main_character')
+
+
+def make_service_hooks_update_groups_action(service):
+    """
+    Make a admin action for the given service
+    :param service: services.hooks.ServicesHook
+    :return: fn to update services groups for the selected users
+    """
+    def update_service_groups(modeladmin, request, queryset):
+        for user in queryset:  # queryset filtering doesn't work here?
+            service.update_groups(user)
+
+    update_service_groups.__name__ = str('update_{}_groups'.format(slugify(service.name)))
+    update_service_groups.short_description = "Sync {} groups for selected users".format(service.title)
+    return update_service_groups
+
+
+class UserAdmin(BaseUserAdmin):
+    """
+    Extending Django's UserAdmin model
+    """
+    def get_actions(self, request):
+        actions = super(BaseUserAdmin, self).get_actions(request)
+
+        for hook in get_hooks('services_hook'):
+            svc = hook()
+            # Check update_groups is redefined/overloaded
+            if svc.update_groups.__module__ != ServicesHook.update_groups.__module__:
+                action = make_service_hooks_update_groups_action(svc)
+                actions[action.__name__] = (action,
+                                            action.__name__,
+                                            action.short_description)
+
+        return actions
+
+# Re-register UserAdmin
+try:
+    admin.site.unregister(User)
+finally:
+    admin.site.register(User, UserAdmin)
