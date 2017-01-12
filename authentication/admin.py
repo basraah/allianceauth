0d15bf4152426649c6375fbb13c8d1f22f92bbb9
Basraah <basraaheve@gmail.com>
2017-01-12 16:20:13 +1000

    Remove obsolete code

--- authentication/admin.py
+++ authentication/admin.py
@@ -23,19 +23,6 @@ class AuthServicesInfoManager(admin.ModelAdmin):
                 pass
         return None
 
-    # TODO: This would be good to convert into a ServiceHook call
-    """
-    def sync_nicknames(self, request, queryset):
-        count = 0
-        for a in queryset:
-            if a.discord_uid != "":
-                update_discord_nickname(a.user.pk)
-                count += 1
-        self.message_user(request, "%s discord accounts queued for nickname sync." % count)
-
-    sync_nicknames.short_description = "Sync nicknames for selected discord accounts"
-    """
-
     search_fields = [
         'user__username',
         'main_char_id',
