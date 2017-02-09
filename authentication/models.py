76a5672940ba6dc730ad8b4af4c91e0ed7e1b36a
Basraah <basraaheve@gmail.com>
2017-02-09 21:26:57 +1000

    Migrate main_char_id to main_character
    Adds a replacement property to fake the original main_char_id field.

--- authentication/models.py
+++ authentication/models.py
@@ -3,6 +3,7 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 from django.contrib.auth.models import User
 from authentication.states import MEMBER_STATE, BLUE_STATE, NONE_STATE
+from eveonline.models import EveCharacter
 
 
 @python_2_unicode_compatible
@@ -16,9 +17,18 @@ class AuthServicesInfo(models.Model):
         (MEMBER_STATE, 'Member'),
     )
 
-    main_char_id = models.CharField(max_length=64, blank=True, default="")
+    main_character = models.ForeignKey(EveCharacter, on_delete=models.SET_NULL, blank=True, null=True, default=None)
     user = models.OneToOneField(User)
     state = models.CharField(blank=True, null=True, choices=STATE_CHOICES, default=NONE_STATE, max_length=10)
+
+    # Faux main_char_id properties for backwards compatibility
+    @property
+    def main_char_id(self):
+        return self.main_character.character_id
+
+    @main_char_id.setter
+    def main_char_id(self, char_id):
+        self.main_character = EveCharacter.objects.get(character_id=char_id)
 
     def __str__(self):
         return self.user.username + ' - AuthInfo'
