ce9f83a77c7693431b83348813aca16a4c429597
Basraah <basraaheve@gmail.com>
2017-05-04 19:17:15 +1000

    Basic name formatter

--- services/hooks.py
+++ services/hooks.py
@@ -1,11 +1,11 @@
 from __future__ import unicode_literals
 
 from django.template.loader import render_to_string
-from django.utils.safestring import mark_safe
+from django.conf import settings
 
 from alliance_auth.hooks import get_hooks
-from authentication.states import MEMBER_STATE, BLUE_STATE
-from authentication.models import AuthServicesInfo
+from eveonline.managers import EveManager
+from services.models import NameFormatConfig
 
 
 class ServicesHook:
@@ -119,3 +119,76 @@ class MenuItemHook:
         return render_to_string(self.template,
                                 {'item': self},
                                 request=request)
+
+
+class NameFormatter:
+
+    DEFAULT_FORMAT = getattr(settings, "DEFAULT_SERVICE_NAME_FORMAT", '[{corp_ticker}] {character_name}')
+
+    def __init__(self, service):
+        """
+        :param service: ServicesHook of the service to generate the name for.
+        """
+        self.service = service
+
+        try:
+            self.format_config = NameFormatConfig.objects.get(service_name=self.service.name)
+        except NameFormatConfig.DoesNotExist:
+            self.format_config = None
+
+    def format_name(self, user):
+        """
+        :param user: django.contrib.auth.models.User to generate name for
+        :return: str Generated name
+        """
+
+        # Get data
+        main_char = EveManager.get_main_character(user)
+        formatter = self._get_formatter(main_char)
+
+        format_data = {
+            'character_name': getattr(main_char, 'character_name',
+                                      user.username if self._default_to_username() else None),
+            'character_id': getattr(main_char, 'character_id', None),
+            'corp_ticker': getattr(main_char, 'corporation_ticker', None),
+            'corp_name': getattr(main_char, 'corporation_name', None),
+            'corp_id': getattr(main_char, 'corporation_id', None),
+            'alliance_name': getattr(main_char, 'alliance_name', None),
+            'alliance_ticker': None,
+            'alliance_id': getattr(main_char, 'alliance_id', None),
+            'username': user.username,
+        }
+
+        if main_char is not None and 'alliance_ticker' in formatter:
+            format_data['alliance_ticker'] = EveManager.get_alliance(main_char.alliance_id).ticker
+
+        return formatter.format(**format_data)
+
+    def _get_formatter(self, character):
+        """
+        Try to get the config format first
+        Then the service default
+        Before finally defaulting to global default
+        :param character: EveCharacter
+        :return: str
+        """
+        fallback_format = self._get_default_formatter()
+        if character is not None and character.in_organisation():
+            # Has a character, and is in organisation
+            return getattr(self.format_config, 'format', fallback_format)
+        else:
+            # No character or not in organisation
+            # Try to use nontenant format first, otherwise use the format and then other defaults
+            return getattr(self.format_config, 'nontenant_format',
+                           getattr(self.format_config, 'format', fallback_format))
+
+    def _get_default_formatter(self):
+        return getattr(self.service, 'name_format', self.DEFAULT_FORMAT)
+
+    def _default_to_username(self):
+        """
+        Default to a users username if they have no main character.
+        Default is True
+        :return: bool
+        """
+        return getattr(self.format_config, 'default_to_username', True)
