642603ceed7f83cfbf7c16782264f40b155894e5
Basraah <basraaheve@gmail.com>
2017-05-06 13:39:08 +1000

    Add manager unit tests

--- alliance_auth/tests/test_settings.py
+++ alliance_auth/tests/test_settings.py
@@ -18,6 +18,7 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 NOSE_ARGS = [
     #'--with-coverage',
     #'--cover-package=',
+    #'--exe',  # If your tests need this to be found/run, check they py files are not chmodded +x
 ]
 
 # Celery configuration
@@ -52,6 +53,7 @@ INSTALLED_APPS = [
     'optimer',
     'corputils',
     'fleetactivitytracking',
+    'fleetup',
     'notifications',
     'esi',
     'permissions_tool',
