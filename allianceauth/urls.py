6a5e97e2418ad5bd8222d50e38715ba03ee46a04
Basraah <basraaheve@gmail.com>
2017-09-22 15:44:00 +1000

    Refactor groupmanagement urls and templates

--- allianceauth/urls.py
+++ allianceauth/urls.py
@@ -1,6 +1,5 @@
 import esi.urls
-import allianceauth.groupmanagement.views
-import allianceauth.notifications.views
+
 import allianceauth.services.views
 from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
@@ -11,6 +10,7 @@ from django.views.generic.base import TemplateView
 import allianceauth.authentication.views
 import allianceauth.authentication.urls
 import allianceauth.notifications.urls
+import allianceauth.groupmanagement.urls
 from allianceauth import NAME
 
 from allianceauth.authentication import hmac_urls
@@ -37,34 +37,16 @@ urlpatterns = [
     url(r'^sso/login$', allianceauth.authentication.views.sso_login, name='auth_sso_login'),
 
     # Notifications
-    url(r'', include(allianceauth.notifications.urls))
+    url(r'', include(allianceauth.notifications.urls)),
+
+    # Groups
+    url(r'', include(allianceauth.groupmanagement.urls)),
 ]
 
 # User viewed/translated URLS
 urlpatterns += i18n_patterns(
     # Group management
-    url(_(r'^groups/'), allianceauth.groupmanagement.views.groups_view, name='auth_groups'),
-    url(_(r'^group/management/'), allianceauth.groupmanagement.views.group_management,
-        name='auth_group_management'),
-    url(_(r'^group/membership/$'), allianceauth.groupmanagement.views.group_membership,
-        name='auth_group_membership'),
-    url(_(r'^group/membership/(\w+)/$'), allianceauth.groupmanagement.views.group_membership_list,
-        name='auth_group_membership_list'),
-    url(_(r'^group/membership/(\w+)/remove/(\w+)/$'), allianceauth.groupmanagement.views.group_membership_remove,
-        name='auth_group_membership_remove'),
-    url(_(r'^group/request_add/(\w+)'), allianceauth.groupmanagement.views.group_request_add,
-        name='auth_group_request_add'),
-    url(_(r'^group/request/accept/(\w+)'), allianceauth.groupmanagement.views.group_accept_request,
-        name='auth_group_accept_request'),
-    url(_(r'^group/request/reject/(\w+)'), allianceauth.groupmanagement.views.group_reject_request,
-        name='auth_group_reject_request'),
 
-    url(_(r'^group/request_leave/(\w+)'), allianceauth.groupmanagement.views.group_request_leave,
-        name='auth_group_request_leave'),
-    url(_(r'group/leave_request/accept/(\w+)'), allianceauth.groupmanagement.views.group_leave_accept_request,
-        name='auth_group_leave_accept_request'),
-    url(_(r'^group/leave_request/reject/(\w+)'), allianceauth.groupmanagement.views.group_leave_reject_request,
-        name='auth_group_leave_reject_request'),
 
     url(_(r'^services/$'), allianceauth.services.views.services_view, name='auth_services'),
 
