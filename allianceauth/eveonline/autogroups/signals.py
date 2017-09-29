d042bbb16e6798ed7776fdf8e90ee79635426e42
Basraah <basraaheve@gmail.com>
2017-09-30 00:52:36 +1000

    Add remaining signals
    Hopefully no more to add?

--- allianceauth/eveonline/autogroups/signals.py
+++ allianceauth/eveonline/autogroups/signals.py
@@ -1,8 +1,9 @@
 import logging
 from django.dispatch import receiver
-from django.db.models.signals import pre_save, pre_delete
-from allianceauth.authentication.models import State, UserProfile
+from django.db.models.signals import pre_save, post_save, pre_delete
+from allianceauth.authentication.models import UserProfile
 from allianceauth.authentication.signals import state_changed
+from allianceauth.eveonline.models import EveCharacter
 
 from .models import AutogroupsConfig
 
@@ -11,12 +12,19 @@ logger = logging.getLogger(__name__)
 
 @receiver(state_changed, sender=UserProfile)
 def profile_state_changed(sender, user, state, **kwargs):
+    """
+    Receives a state change and triggers a group update for that user
+    """
     logger.debug("Received state_changed from %s to state %s" % (user, state))
-    AutogroupsConfig.objects.update_groups_for_user(state, user)
+    AutogroupsConfig.objects.update_groups_for_user(user, state)
 
 
 @receiver(pre_save, sender=AutogroupsConfig)
-def pre_save_user(sender, instance, *args, **kwargs):
+def pre_save_config(sender, instance, *args, **kwargs):
+    """
+    Checks if enable was toggled on group config and
+    deletes groups if necessary.
+    """
     logger.debug("Received pre_save from {}".format(instance))
     if not instance.pk:
         # new model being created
@@ -33,7 +41,38 @@ def pre_save_user(sender, instance, *args, **kwargs):
     except AutogroupsConfig.DoesNotExist:
         pass
 
-# TODO Delete config signal
 
-# TODO Signal: change main character but state doesnt change
-# TODO Signal: change main character MODEL but state doesnt change
+@receiver(pre_delete, sender=AutogroupsConfig)
+def pre_delete_config(sender, instance, *args, **kwargs):
+    """
+    Delete groups on deleting config
+    """
+    instance.delete_corp_managed_groups()
+    instance.delete_alliance_managed_groups()
+
+
+@receiver(post_save, sender=EveCharacter)
+def check_groups_on_character_update(sender, instance, *args, **kwargs):
+    """
+    Trigger check when main character model changes but state does not change.
+    This signal will cause the group update to run twice if the
+    users state changes as well. No easy way to suppress that
+    """
+    try:
+        AutogroupsConfig.objects.update_groups_for_user(instance.userprofile.user)
+    except UserProfile.DoesNotExist:
+        # Not a main character
+        pass
+
+
+@receiver(pre_save, sender=UserProfile)
+def check_groups_on_main_character_update(sender, instance, created, *args, **kwargs):
+    """
+    Trigger check when main character changes but state does not change.
+    This signal will cause the group update to run twice if the
+    users state changes as well. No easy way to suppress that
+    """
+    if not created:
+        update_fields = kwargs.pop('update_fields', []) or []
+        if 'main_character' in update_fields:
+            AutogroupsConfig.objects.update_groups_for_user(instance.user)
