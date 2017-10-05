54a78ba12e0d7d538b00958607c93fe77f7dc046
Basraah <basraaheve@gmail.com>
2017-10-05 13:04:16 +1000

    DRY night mode state

--- allianceauth/views.py
+++ allianceauth/views.py
@@ -6,5 +6,9 @@ class NightModeRedirectView(View):
     SESSION_VAR = 'NIGHT_MODE'
 
     def get(self, request, *args, **kwargs):
-        request.session[self.SESSION_VAR] = not request.session.get(self.SESSION_VAR, False)
+        request.session[self.SESSION_VAR] = not self.night_mode_state(request)
         return HttpResponseRedirect(request.GET.get('next', '/'))
+
+    @classmethod
+    def night_mode_state(cls, request):
+        return request.session.get(cls.SESSION_VAR, False)
