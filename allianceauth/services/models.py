9c5395041b7e39fe4c2699616c545a1770d6ac25
Basraah <basraaheve@gmail.com>
2017-10-08 00:18:32 +1000

    Move admin model

--- allianceauth/services/models.py
+++ allianceauth/services/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from django.contrib import admin
-from django import forms
-from allianceauth import hooks
+
 from allianceauth.authentication.models import State
 
 
@@ -17,17 +15,3 @@ class NameFormatConfig(models.Model):
                                                      "formatter for each state for each service.")
 
 
-class NameFormatConfigForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(NameFormatConfigForm, self).__init__(*args, **kwargs)
-        SERVICE_CHOICES = [(s.name, s.name) for h in hooks.get_hooks('services_hook') for s in [h()]]
-        if self.instance.id:
-            SERVICE_CHOICES.append((self.instance.field, self.instance.field))
-        self.fields['service_name'] = forms.ChoiceField(choices=SERVICE_CHOICES)
-
-
-class NameFormatConfigAdmin(admin.ModelAdmin):
-    form = NameFormatConfigForm
-
-
-admin.site.register(NameFormatConfig, NameFormatConfigAdmin)
