91e78b3a41ab069f8c6526a98c3bce014c54a0d1
Basraah <basraaheve@gmail.com>
2017-10-11 13:18:08 +1000

    Fix wonky merge

--- allianceauth/context_processors.py
+++ allianceauth/context_processors.py
@@ -4,7 +4,6 @@ from .views import NightModeRedirectView
 
 def auth_settings(request):
     return {
-        'DOMAIN': settings.DOMAIN,
         'SITE_NAME': settings.SITE_NAME,
         'NIGHT_MODE': NightModeRedirectView.night_mode_state(request),
     }
