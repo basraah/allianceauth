f9e2c561970a4622bce4c93db1aa3082d464360b
Basraah <basraaheve@gmail.com>
2017-11-24 13:01:23 +1000

    Prevent whitespace being stripped from group names

--- allianceauth/eveonline/autogroups/admin.py
+++ allianceauth/eveonline/autogroups/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db import models
 from .models import AutogroupsConfig
 
 import logging
@@ -7,13 +8,17 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def sync_nickname(modeladmin, request, queryset):
+def sync_user_groups(modeladmin, request, queryset):
     for agc in queryset:
         logger.debug("update_all_states_group_membership for {}".format(agc))
         agc.update_all_states_group_membership()
 
 
 class AutogroupsConfigAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.CharField: {'strip': False}
+    }
+
     def get_readonly_fields(self, request, obj=None):
         if obj:  # This is the case when obj is already created i.e. it's an edit
             return [
@@ -25,7 +30,7 @@ class AutogroupsConfigAdmin(admin.ModelAdmin):
 
     def get_actions(self, request):
         actions = super(AutogroupsConfigAdmin, self).get_actions(request)
-        actions['sync_user_groups'] = (sync_nickname,
+        actions['sync_user_groups'] = (sync_user_groups,
                                        'sync_user_groups',
                                        'Sync all users groups for this Autogroup Config')
         return actions
